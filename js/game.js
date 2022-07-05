1b50e114703211f9ef385952c1a65cd6e1d569ac
Kacper Adamski <kacper.adamski95@gmail.com>
2022-07-05 22:32:35 +0200

    added some js files, finished with basic version

--- js/game.js
+++ js/game.js
@@ -1,9 +1,21 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
 
 let lastRenderTime = 0;
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
+    if (gameOver) {
+        if (confirm('You lost. Your score is: Press ok to restart.')) {
+            window.location = '/'
+        }
+        return
+    }
+
+
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -19,9 +31,16 @@ window.requestAnimationFrame(main);
 
 function update() {
     updateSnake()
+    updateFood()
+    checkDeath()
 }
 
 function draw() {
     gameBoard.innerHTML = ''
     drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
